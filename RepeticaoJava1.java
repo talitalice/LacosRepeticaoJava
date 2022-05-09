3401f6f30e78957d029466770e35c21f2f0b6a1c
Talita Alice <talitalionsmane@gmail.com>
2022-05-09 14:21:13 -0300

    repeticao java

---
+++ RepeticaoJava1.java
@@ -0,0 +1,16 @@
+package PacoteJava;
+
+public class RepeticaoJava
+{
+
+	public static void main(String[] args)
+	{
+		for(int x = 1000; x <= 1999; x++)
+		{
+			if(x%11==5)
+				System.out.println(x);
+		}
+
+	}
+
+}
